7a896096868534ac523bd15b8dc1cf1e19fd0546
sofia Luna <Sofia21luna@gmail.com>
2020-10-28 16:40:02 -0600

    middleware for validating post by id is working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const Posts = require("./postDb");
 
@@ -16,8 +17,10 @@ router.get('/', (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => { // validatePostId
+router.get('/:id', validatePostId, (req, res) => { // validatePostId
   // do your magic!
+  res.status(200).json(req.post);
+
 });
 
 router.post('/', (req, res) => { // validatePost
@@ -36,6 +39,24 @@ router.put('/:id', (req, res) => { // validatePostId
 
 function validatePostId(req, res, next) {
   // do your magic!
+
+  const { id } = req.params
+
+  Posts.getById(id)
+  .then(data => {
+    if (data) {
+      req.post = data
+
+      next()
+    } else {
+      res.status(400).json({message: 'there is no post with id ' + id})
+    }
+  })
+  .catch(error => {
+    console.log(error.message)
+    // res.status(500).json({ message: 'something bad happened' })
+    next({ code: 500, message: 'Something crashed and burned'})
+  })
 }
 
 module.exports = router;
