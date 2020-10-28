9e618e610592755673ccb37cc7fe29e82ec906d0
sofia Luna <Sofia21luna@gmail.com>
2020-10-28 14:08:18 -0600

    get endpoints completed

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const userRouter = require('./users/userRouter')
 
 const server = express();
+
+server.use(morgan('dev'));
+server.use(helmet())
+server.use(logger)
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +18,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
-
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}${req.method} ${req.url}]`
+  );
+  next()
+}
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,59 @@
-const express = require('express');
+const express = require("express");
+
+const Users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  Users.get(req.query)
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving users",
+      });
+    });
+});
+
+router.get("/:id", validateUserId, (req, res) => {
+  // do your magic!
+  res.status(200).json(req.user);
+});
+
+router.get("/:id/posts", (req, res) => {
+  // do your magic!
+  const { id } = req.params;
+  Users.getUserPosts(id)
+    .then((posts) => {
+      if (posts) {
+        res.status(200).json(posts);
+      } else {
+        res.status(404).json({ message: "Post not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: error.message, stack: error.stack
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
@@ -34,6 +61,21 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+
+  Users.getById(id)
+    .then((data) => {
+      if (data) {
+        req.user = data;
+        next();
+      } else {
+        res.status(400).json({ message: "Invalid user id" });
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(404).json({ message: "Id not found!" });
+    });
 }
 
 function validateUser(req, res, next) {
