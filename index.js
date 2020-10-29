0c6aee73f01f1066f8c2dd71d768e6a705de621f
sofia Luna <Sofia21luna@gmail.com>
2020-10-29 14:15:55 -0600

    code is ready for deployment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+
+require('dotenv').config()
 const server = require('./server.js');
 
-server.listen(4040, () => {
-  console.log('\n* Server Running on http://localhost:4040 *\n');
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`\n Server running in port ${PORT}`);
 });
